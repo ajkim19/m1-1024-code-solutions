ee8c4fe6b50c9dfae954c6db19b5afe080e4ee90
Aaron Kim <ajkim19@gmail.com>
2024-12-18 04:49:19 +0000

    Completed dom-event exercises

---
+++ dom-events/buttons.js
@@ -0,0 +1,29 @@
+'use strict';
+// Click Event
+const classClickButton = document.querySelector('.click-button');
+if (!classClickButton) throw new Error('classClickButton does not exist');
+function handleClick(event) {
+  console.log('button clicked');
+  console.dir(event);
+  console.log(event.target);
+}
+classClickButton.addEventListener('click', handleClick);
+// Mouseover Event
+const classHoverButton = document.querySelector('.hover-button');
+if (!classHoverButton) throw new Error('classHoverButton does not exist');
+function handleMouseover(event) {
+  console.log('button hovered');
+  console.dir(event);
+  console.log(event.target);
+}
+classHoverButton.addEventListener('mouseover', handleMouseover);
+// Double-click Event
+const classDoubleClickButton = document.querySelector('.double-click-button');
+if (!classDoubleClickButton)
+  throw new Error('classDoubleClickButton does not exist');
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.dir(event);
+  console.log(event.target);
+}
+classDoubleClickButton.addEventListener('dblclick', handleDoubleClick);
