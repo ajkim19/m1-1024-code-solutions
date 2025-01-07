082d8ba79f5a30e0d48983776a30048c3f18797f
Aaron Kim <ajkim19@gmail.com>
2025-01-07 04:41:02 +0000

    Added books object

---
+++ typescript-and-json/main.ts
@@ -0,0 +1,20 @@
+const books: object[] = [
+  {
+    isbn: '978-0141439600',
+    title: 'A Tale of Two Cities',
+    author: 'Charles Dickens',
+  },
+  {
+    isbn: '978-1607963028',
+    title: 'The Little Prince',
+    author: 'Antoine de Saint-Exupéry',
+  },
+  {
+    isbn: '9780061122415',
+    title: 'The Alchemist',
+    author: 'Paulo Coelho',
+  },
+];
+
+console.log('books:', books);
+console.log('typeof books:', typeof books);
