2540a6a8567b7ef2c26d6deb6a1ea3c3f8aad83c
Aaron Kim <ajkim19@gmail.com>
2025-01-10 23:12:07 +0000

    Added text content
    Used main.ts to add text content to the page. Each character of text is placed between span tags using the renderLetter function.

--- typescript-typing-tutor/main.js
+++ typescript-typing-tutor/main.js
@@ -1,16 +1,14 @@
+'use strict';
 // Encases each letter of a string in a 'span' tag
-function renderLetter(char: string): HTMLSpanElement {
+function renderLetter(char) {
   const $spanChar = document.createElement('span');
   $spanChar.className = 'letter';
   $spanChar.textContent = char;
   return $spanChar;
 }
-
 const $wordString = document.querySelector('.word-string');
 if (!$wordString) throw new Error('$wordString does not exist');
-
 const words = 'grumpy wizards make toxic brew';
-
 for (let i = 0; i < words.length; i++) {
   const $spanLetter = renderLetter(words[i]);
   $wordString.append($spanLetter);
