0b76713180c3093f2a87bf3bbb2f2064a76c025f
Aaron Kim <ajkim19@gmail.com>
2025-01-11 06:54:20 +0000

    Added accuracy score

--- typescript-typing-tutor/main.js
+++ typescript-typing-tutor/main.js
@@ -1,4 +1,6 @@
 'use strict';
+const $accuracy = document.querySelector('.accuracy');
+if (!$accuracy) throw new Error('$accuracy does not exist');
 // Encases each letter of a string in a 'span' tag
 function renderLetter(char) {
   const $spanChar = document.createElement('span');
@@ -18,25 +20,46 @@ function toType(string) {
     $wordString.append($wordChar);
   }
 }
-function typingFocus(spanTag) {
-  spanTag.style.textDecoration = 'none';
-}
 // Sets up the start of the application
-const words = 'My dog is a stubborn B.';
+const words = 'my dog is stubborn';
 toType(words);
 const $spanLetters = document.querySelectorAll('span.letter');
 if (!$spanLetters) throw new Error('spanLetters does not exist');
+// Sets up to measure accuracy of typing
+let correctKeys = 0;
+const possibleKeys = words.length;
+const accuracyArray = [];
 let charNum = 0;
 document.addEventListener('keydown', (event) => {
   if (event.key === $spanLetters[charNum].textContent) {
-    console.log(`You pressed the right key - ${event.key}`);
     $spanLetters[charNum].style.textDecoration = 'none';
     $spanLetters[charNum].style.color = 'rgb(0, 225, 0)';
     if ($spanLetters[charNum + 1]) {
       $spanLetters[charNum + 1].style.textDecoration = 'underline';
     }
+    if (accuracyArray.length < charNum + 1) {
+      accuracyArray.push(1);
+      console.log(accuracyArray, 'correct');
+      if (accuracyArray.length === possibleKeys) {
+        // Calculates accuracy of typing
+        const initialValue = 0;
+        const correctKeysTotal = accuracyArray.reduce(
+          (toAdd, currentSum) => toAdd + currentSum,
+          initialValue
+        );
+        const accuracyFinal = Math.floor(
+          (correctKeysTotal / possibleKeys) * 100
+        );
+        // Displays the final accuracy calculation
+        $accuracy.textContent = `Your accuracy score is ${accuracyFinal}%`;
+      }
+    }
     charNum++;
   } else {
     $spanLetters[charNum].style.color = 'red';
+    if (accuracyArray.length < charNum + 1) {
+      accuracyArray.push(0);
+      console.log(accuracyArray.length);
+    }
   }
 });
--- typescript-typing-tutor/main.ts
+++ typescript-typing-tutor/main.ts
@@ -1,3 +1,6 @@
+const $accuracy = document.querySelector('.accuracy') as HTMLDivElement;
+if (!$accuracy) throw new Error('$accuracy does not exist');
+
 // Encases each letter of a string in a 'span' tag
 function renderLetter(char: string): HTMLSpanElement {
   const $spanChar = document.createElement('span');
@@ -20,23 +23,48 @@ function toType(string: string): void {
 }
 
 // Sets up the start of the application
-const words = 'My dog is a stubborn B.';
+const words = 'my dog is stubborn';
 toType(words);
 const $spanLetters: NodeListOf<HTMLSpanElement> =
   document.querySelectorAll('span.letter');
 if (!$spanLetters) throw new Error('spanLetters does not exist');
 
+// Sets up to measure accuracy of typing
+const possibleKeys = words.length;
+const accuracyArray: number[] = [];
+
 let charNum = 0;
 document.addEventListener('keydown', (event: KeyboardEvent) => {
   if (event.key === $spanLetters[charNum].textContent) {
-    console.log(`You pressed the right key - ${event.key}`);
     $spanLetters[charNum].style.textDecoration = 'none';
     $spanLetters[charNum].style.color = 'rgb(0, 225, 0)';
     if ($spanLetters[charNum + 1]) {
       $spanLetters[charNum + 1].style.textDecoration = 'underline';
     }
+    if (accuracyArray.length < charNum + 1) {
+      accuracyArray.push(1);
+      console.log(accuracyArray, 'correct');
+      if (accuracyArray.length === possibleKeys) {
+        // Calculates accuracy of typing
+        const initialValue = 0;
+        const correctKeysTotal = accuracyArray.reduce(
+          (toAdd, currentSum) => toAdd + currentSum,
+          initialValue
+        );
+        const accuracyFinal = Math.floor(
+          (correctKeysTotal / possibleKeys) * 100
+        );
+
+        // Displays the final accuracy calculation
+        $accuracy.textContent = `Your accuracy score is ${accuracyFinal}%`;
+      }
+    }
     charNum++;
   } else {
     $spanLetters[charNum].style.color = 'red';
+    if (accuracyArray.length < charNum + 1) {
+      accuracyArray.push(0);
+      console.log(accuracyArray.length);
+    }
   }
 });
