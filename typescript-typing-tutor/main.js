fd94a0e156596eb475eddb664256f2ef46cf547b
Aaron Kim <ajkim19@gmail.com>
2025-01-12 05:54:24 +0000

    Added keyboard elements
    div elements were added to the keyboard image in preparation for keyboard key hints.

--- typescript-typing-tutor/main.js
+++ typescript-typing-tutor/main.js
@@ -1,6 +1,18 @@
 'use strict';
+const $wordString = document.querySelector('.word-string');
+if (!$wordString) throw new Error('$wordString does not exist');
 const $accuracy = document.querySelector('.accuracy');
 if (!$accuracy) throw new Error('$accuracy does not exist');
+const $numbersRow = document.querySelector('.numbers-row');
+if (!$numbersRow) throw new Error('$numbersRow does not exist');
+const $topRow = document.querySelector('.top-row');
+if (!$topRow) throw new Error('$topRow does not exist');
+const $middleRow = document.querySelector('.middle-row');
+if (!$middleRow) throw new Error('$middleRow does not exist');
+const $bottomRow = document.querySelector('.bottom-row');
+if (!$bottomRow) throw new Error('$bottomRow does not exist');
+const $spaceRow = document.querySelector('.space-row');
+if (!$spaceRow) throw new Error('$spaceRow does not exist');
 // Encases each letter of a string in a 'span' tag
 function renderLetter(char) {
   const $spanChar = document.createElement('span');
@@ -10,7 +22,6 @@ function renderLetter(char) {
 }
 // Places each character in words between span tags
 function toType(string) {
-  const $wordString = document.querySelector('.word-string');
   if (!$wordString) throw new Error('$wordString does not exist');
   for (let i = 0; i < string.length; i++) {
     const $wordChar = renderLetter(string[i]);
@@ -20,18 +31,28 @@ function toType(string) {
     $wordString.append($wordChar);
   }
 }
+function resetExercise() {
+  if (!$wordString) throw new Error('$wordString does not exist');
+  if (!$accuracy) throw new Error('$accuracy does not exist');
+  $wordString.innerHTML = '';
+  $accuracy.innerHTML = '';
+  charNum = 0;
+  accuracyArray.length = 0;
+  toType(words);
+}
 // Sets up the start of the application
-const words = 'my dog is stubborn';
+const words = 'My dog is being stubborn.';
 toType(words);
 const $spanLetters = document.querySelectorAll('span.letter');
 if (!$spanLetters) throw new Error('spanLetters does not exist');
 // Sets up to measure accuracy of typing
-let correctKeys = 0;
 const possibleKeys = words.length;
 const accuracyArray = [];
 let charNum = 0;
 document.addEventListener('keydown', (event) => {
-  if (event.key === $spanLetters[charNum].textContent) {
+  if (event.key === 'Shift') {
+    return;
+  } else if (event.key === $spanLetters[charNum].textContent) {
     $spanLetters[charNum].style.textDecoration = 'none';
     $spanLetters[charNum].style.color = 'rgb(0, 225, 0)';
     if ($spanLetters[charNum + 1]) {
@@ -39,7 +60,6 @@ document.addEventListener('keydown', (event) => {
     }
     if (accuracyArray.length < charNum + 1) {
       accuracyArray.push(1);
-      console.log(accuracyArray, 'correct');
       if (accuracyArray.length === possibleKeys) {
         // Calculates accuracy of typing
         const initialValue = 0;
@@ -47,11 +67,17 @@ document.addEventListener('keydown', (event) => {
           (toAdd, currentSum) => toAdd + currentSum,
           initialValue
         );
-        const accuracyFinal = Math.floor(
+        const accuracyScore = Math.floor(
           (correctKeysTotal / possibleKeys) * 100
         );
         // Displays the final accuracy calculation
-        $accuracy.textContent = `Your accuracy score is ${accuracyFinal}%`;
+        $accuracy.innerHTML = `You were&nbsp;<div class="accuracy-score">${accuracyScore}%</div>&nbsp;accurate with your typing`;
+        setTimeout(() => {
+          if (confirm('Would you like to try again?')) {
+            resetExercise();
+            return;
+          }
+        }, 0);
       }
     }
     charNum++;
@@ -59,7 +85,76 @@ document.addEventListener('keydown', (event) => {
     $spanLetters[charNum].style.color = 'red';
     if (accuracyArray.length < charNum + 1) {
       accuracyArray.push(0);
-      console.log(accuracyArray.length);
     }
   }
 });
+const keyboardKeys = [
+  '`',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+  '-',
+  '=',
+  'q',
+  'w',
+  'e',
+  'r',
+  't',
+  'y',
+  'u',
+  'i',
+  'o',
+  'p',
+  '[',
+  ']',
+  '\\',
+  'a',
+  's',
+  'd',
+  'f',
+  'g',
+  'h',
+  'j',
+  'k',
+  'l',
+  ';',
+  `'`,
+  'Shift',
+  'z',
+  'x',
+  'c',
+  'v',
+  'b',
+  'n',
+  'm',
+  ',',
+  '.',
+  '/',
+  'Shift',
+  ' ',
+];
+for (let i = 0; i < keyboardKeys.length; i++) {
+  const $divElement = document.createElement('div');
+  $divElement.className = `keyboard-key ${keyboardKeys[i]} flex`;
+  const $h1Element = document.createElement('h1');
+  $h1Element.textContent = keyboardKeys[i].toLocaleUpperCase();
+  $divElement.append($h1Element);
+  if (i < 13 && $numbersRow) {
+    $numbersRow.append($divElement);
+  } else if (i >= 13 && i < 26 && $topRow) {
+    $topRow.append($divElement);
+  } else if (i >= 26 && i < 37 && $middleRow) {
+    $middleRow.append($divElement);
+  } else if (i >= 37 && i < 49 && $bottomRow) {
+    $bottomRow.append($divElement);
+  } else {
+    $spaceRow.append($divElement);
+  }
+}
--- typescript-typing-tutor/main.ts
+++ typescript-typing-tutor/main.ts
@@ -1,5 +1,17 @@
-const $accuracy = document.querySelector('.accuracy') as HTMLDivElement;
+const $wordString = document.querySelector<HTMLDivElement>('.word-string');
+if (!$wordString) throw new Error('$wordString does not exist');
+const $accuracy = document.querySelector<HTMLDivElement>('.accuracy');
 if (!$accuracy) throw new Error('$accuracy does not exist');
+const $numbersRow = document.querySelector<HTMLDivElement>('.numbers-row');
+if (!$numbersRow) throw new Error('$numbersRow does not exist');
+const $topRow = document.querySelector<HTMLDivElement>('.top-row');
+if (!$topRow) throw new Error('$topRow does not exist');
+const $middleRow = document.querySelector<HTMLDivElement>('.middle-row');
+if (!$middleRow) throw new Error('$middleRow does not exist');
+const $bottomRow = document.querySelector<HTMLDivElement>('.bottom-row');
+if (!$bottomRow) throw new Error('$bottomRow does not exist');
+const $spaceRow = document.querySelector<HTMLDivElement>('.space-row');
+if (!$spaceRow) throw new Error('$spaceRow does not exist');
 
 // Encases each letter of a string in a 'span' tag
 function renderLetter(char: string): HTMLSpanElement {
@@ -11,7 +23,6 @@ function renderLetter(char: string): HTMLSpanElement {
 
 // Places each character in words between span tags
 function toType(string: string): void {
-  const $wordString = document.querySelector('.word-string');
   if (!$wordString) throw new Error('$wordString does not exist');
   for (let i = 0; i < string.length; i++) {
     const $wordChar = renderLetter(string[i]);
@@ -22,8 +33,18 @@ function toType(string: string): void {
   }
 }
 
+function resetExercise(): void {
+  if (!$wordString) throw new Error('$wordString does not exist');
+  if (!$accuracy) throw new Error('$accuracy does not exist');
+  $wordString.innerHTML = '';
+  $accuracy.innerHTML = '';
+  charNum = 0;
+  accuracyArray.length = 0;
+  toType(words);
+}
+
 // Sets up the start of the application
-const words = 'my dog is stubborn';
+const words = 'My dog is being stubborn.';
 toType(words);
 const $spanLetters: NodeListOf<HTMLSpanElement> =
   document.querySelectorAll('span.letter');
@@ -35,7 +56,9 @@ const accuracyArray: number[] = [];
 
 let charNum = 0;
 document.addEventListener('keydown', (event: KeyboardEvent) => {
-  if (event.key === $spanLetters[charNum].textContent) {
+  if (event.key === 'Shift') {
+    console.log('');
+  } else if (event.key === $spanLetters[charNum].textContent) {
     $spanLetters[charNum].style.textDecoration = 'none';
     $spanLetters[charNum].style.color = 'rgb(0, 225, 0)';
     if ($spanLetters[charNum + 1]) {
@@ -43,7 +66,6 @@ document.addEventListener('keydown', (event: KeyboardEvent) => {
     }
     if (accuracyArray.length < charNum + 1) {
       accuracyArray.push(1);
-      console.log(accuracyArray, 'correct');
       if (accuracyArray.length === possibleKeys) {
         // Calculates accuracy of typing
         const initialValue = 0;
@@ -51,12 +73,17 @@ document.addEventListener('keydown', (event: KeyboardEvent) => {
           (toAdd, currentSum) => toAdd + currentSum,
           initialValue
         );
-        const accuracyFinal = Math.floor(
+        const accuracyScore = Math.floor(
           (correctKeysTotal / possibleKeys) * 100
         );
 
         // Displays the final accuracy calculation
-        $accuracy.textContent = `Your accuracy score is ${accuracyFinal}%`;
+        $accuracy.innerHTML = `You were&nbsp;<div class="accuracy-score">${accuracyScore}%</div>&nbsp;accurate with your typing`;
+        setTimeout(() => {
+          if (confirm('Would you like to try again?')) {
+            resetExercise();
+          }
+        }, 0);
       }
     }
     charNum++;
@@ -64,7 +91,78 @@ document.addEventListener('keydown', (event: KeyboardEvent) => {
     $spanLetters[charNum].style.color = 'red';
     if (accuracyArray.length < charNum + 1) {
       accuracyArray.push(0);
-      console.log(accuracyArray.length);
     }
   }
 });
+
+const keyboardKeys: string[] = [
+  '`',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+  '-',
+  '=',
+  'q',
+  'w',
+  'e',
+  'r',
+  't',
+  'y',
+  'u',
+  'i',
+  'o',
+  'p',
+  '[',
+  ']',
+  '\\',
+  'a',
+  's',
+  'd',
+  'f',
+  'g',
+  'h',
+  'j',
+  'k',
+  'l',
+  ';',
+  `'`,
+  'Shift',
+  'z',
+  'x',
+  'c',
+  'v',
+  'b',
+  'n',
+  'm',
+  ',',
+  '.',
+  '/',
+  'Shift',
+  ' ',
+];
+
+for (let i = 0; i < keyboardKeys.length; i++) {
+  const $divElement = document.createElement('div');
+  $divElement.className = `keyboard-key ${keyboardKeys[i]} flex`;
+  const $h1Element = document.createElement('h1');
+  $h1Element.textContent = keyboardKeys[i].toLocaleUpperCase();
+  $divElement.append($h1Element);
+  if (i < 13 && $numbersRow) {
+    $numbersRow.append($divElement);
+  } else if (i >= 13 && i < 26 && $topRow) {
+    $topRow.append($divElement);
+  } else if (i >= 26 && i < 37 && $middleRow) {
+    $middleRow.append($divElement);
+  } else if (i >= 37 && i < 49 && $bottomRow) {
+    $bottomRow.append($divElement);
+  } else {
+    $spaceRow.append($divElement);
+  }
+}
