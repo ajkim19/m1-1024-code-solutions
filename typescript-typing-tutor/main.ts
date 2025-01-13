43c3a82e4436ea42f7efa4e6d71edc29649e8702
Aaron Kim <ajkim19@gmail.com>
2025-01-13 21:46:59 +0000

    Added keyboard
    An keyboard was added that will indicate to the user where on their keyboard to press.

--- typescript-typing-tutor/main.ts
+++ typescript-typing-tutor/main.ts
@@ -12,6 +12,58 @@ const $bottomRow = document.querySelector<HTMLDivElement>('.bottom-row');
 if (!$bottomRow) throw new Error('$bottomRow does not exist');
 const $spaceRow = document.querySelector<HTMLDivElement>('.space-row');
 if (!$spaceRow) throw new Error('$spaceRow does not exist');
+const keyboardKey = [
+  'grave-accent',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+  'dash',
+  'equal',
+  'q',
+  'w',
+  'e',
+  'r',
+  't',
+  'y',
+  'u',
+  'i',
+  'o',
+  'p',
+  'l-bracket',
+  'r-bracket',
+  'backslash',
+  'a',
+  's',
+  'd',
+  'f',
+  'g',
+  'h',
+  'j',
+  'k',
+  'l',
+  'semicolon',
+  'apostrophe',
+  'shift-l',
+  'z',
+  'x',
+  'c',
+  'v',
+  'b',
+  'n',
+  'm',
+  'comma',
+  'period',
+  'slash',
+  'shift-r',
+  'space',
+];
 
 // Encases each letter of a string in a 'span' tag
 function renderLetter(char: string): HTMLSpanElement {
@@ -24,6 +76,7 @@ function renderLetter(char: string): HTMLSpanElement {
 // Places each character in words between span tags
 function toType(string: string): void {
   if (!$wordString) throw new Error('$wordString does not exist');
+  $wordString.innerHTML = '';
   for (let i = 0; i < string.length; i++) {
     const $wordChar = renderLetter(string[i]);
     if (i === 0) {
@@ -33,12 +86,126 @@ function toType(string: string): void {
   }
 }
 
+function addKeyboardKey(lettersList: string[]): void {
+  for (let i = 0; i < lettersList.length; i++) {
+    const $keyDivElement = document.createElement('div');
+    if (!$keyDivElement) throw new Error('$keyDivElement does not exist');
+    $keyDivElement.className = `keyboard-key ${lettersList[i]} flex`;
+    const $h1Element = document.createElement('h1');
+    if (lettersList[i] === 'grave-accent') {
+      $h1Element.textContent = '`';
+    } else if (lettersList[i] === 'dash') {
+      $h1Element.textContent = '-';
+    } else if (lettersList[i] === 'equal') {
+      $h1Element.textContent = '=';
+    } else if (lettersList[i] === 'l-bracket') {
+      $h1Element.textContent = '[';
+    } else if (lettersList[i] === 'r-bracket') {
+      $h1Element.textContent = ']';
+    } else if (lettersList[i] === 'backslash') {
+      $h1Element.textContent = '\\';
+    } else if (lettersList[i] === 'semicolon') {
+      $h1Element.textContent = ';';
+    } else if (lettersList[i] === 'apostrophe') {
+      $h1Element.textContent = "'";
+    } else if (lettersList[i] === 'comma') {
+      $h1Element.textContent = ',';
+    } else if (lettersList[i] === 'period') {
+      $h1Element.textContent = '.';
+    } else if (lettersList[i] === 'slash') {
+      $h1Element.textContent = '/';
+    } else if (lettersList[i] === 'shift-l' || lettersList[i] === 'shift-r') {
+      $h1Element.textContent = 'SHIFT';
+    } else if (lettersList[i] === 'space') {
+      $h1Element.textContent = 'SPACE';
+    } else {
+      $h1Element.textContent = lettersList[i].toLocaleUpperCase();
+    }
+    $keyDivElement.append($h1Element);
+    if (i < 13 && $numbersRow) {
+      $numbersRow.append($keyDivElement);
+    } else if (i >= 13 && i < 26 && $topRow) {
+      $topRow.append($keyDivElement);
+    } else if (i >= 26 && i < 37 && $middleRow) {
+      $middleRow.append($keyDivElement);
+    } else if (i >= 37 && i < 49 && $bottomRow) {
+      $bottomRow.append($keyDivElement);
+    } else if ($spaceRow) {
+      $spaceRow.append($keyDivElement);
+    }
+  }
+}
+
+// Styles the key to be pressed on the keyboard image
+function keyboardHintOn(charNum: number): void {
+  const $spanLetter = $spanLetters[charNum] as HTMLSpanElement;
+  if (!$spanLetter) throw new Error('$spanLetter does not exist');
+  let spanLettersText = $spanLetter.textContent;
+  if (spanLettersText === '`') {
+    spanLettersText = 'grave-accent';
+  } else if (spanLettersText === '[') {
+    spanLettersText = 'l-bracket';
+  } else if (spanLettersText === ']') {
+    spanLettersText = 'r-bracket';
+  } else if (spanLettersText === '\\') {
+    spanLettersText = 'backslash';
+  } else if (spanLettersText === ';') {
+    spanLettersText = 'semicolon';
+  } else if (spanLettersText === "'") {
+    spanLettersText = 'apostrophe';
+  } else if (spanLettersText === ',') {
+    spanLettersText = 'comma';
+  } else if (spanLettersText === '.') {
+    spanLettersText = 'period';
+  } else if (spanLettersText === '/') {
+    spanLettersText = 'slash';
+  } else if (spanLettersText === ' ') {
+    spanLettersText = 'space';
+  }
+  if (!spanLettersText) throw new Error('spanLettersText does not exist');
+  const $keyboardKey = document.querySelector(
+    `.${spanLettersText.toLocaleLowerCase()}`
+  );
+  if (!$keyboardKey) throw new Error('$keyboardKey does not exist');
+  console.log('$keyboardKey', $keyboardKey);
+
+  // Makes the key on the keyboard bold and blue
+  const $keyToPress = $keyboardKey.children[0] as HTMLElement;
+  if (!$keyToPress) throw new Error('$keyToPress does not exist');
+  $keyToPress.style.fontWeight = '900';
+  $keyToPress.style.color = 'rgb(21, 0, 255)';
+}
+
+function keyboardHintOff(charNum: number): void {
+  const $spanLetter = $spanLetters[charNum] as HTMLSpanElement;
+  if (!$spanLetter) throw new Error('$spanLetter does not exist');
+  let $spanLettersText = $spanLetter.textContent;
+  if (!$spanLettersText) throw new Error('$spanLettersText does not exist');
+  if ($spanLettersText === ' ') {
+    $spanLettersText = 'space';
+  } else if ($spanLettersText === '.') {
+    $spanLettersText = 'period';
+  }
+  console.log('$spanLettersText', $spanLettersText);
+  const $keyboardKey = document.querySelector(
+    `.${$spanLettersText.toLocaleLowerCase()}`
+  );
+  if (!$keyboardKey) throw new Error('$keyboardKey does not exist');
+  console.log('$keyboardKey', $keyboardKey);
+
+  // Makes the key on the keyboard bold and blue
+  const $keyToPress = $keyboardKey.children[0] as HTMLElement;
+  if (!$keyToPress) throw new Error('$keyToPress does not exist');
+  $keyToPress.style.fontWeight = '500';
+  $keyToPress.style.color = 'black';
+}
+
 function resetExercise(): void {
   if (!$wordString) throw new Error('$wordString does not exist');
   if (!$accuracy) throw new Error('$accuracy does not exist');
   $wordString.innerHTML = '';
   $accuracy.innerHTML = '';
-  charNum = 0;
+  currentPosition = 0;
   accuracyArray.length = 0;
   toType(words);
 }
@@ -46,25 +213,52 @@ function resetExercise(): void {
 // Sets up the start of the application
 const words = 'My dog is being stubborn.';
 toType(words);
-const $spanLetters: NodeListOf<HTMLSpanElement> =
-  document.querySelectorAll('span.letter');
-if (!$spanLetters) throw new Error('spanLetters does not exist');
 
 // Sets up to measure accuracy of typing
 const possibleKeys = words.length;
 const accuracyArray: number[] = [];
 
-let charNum = 0;
+// Adds new keys to keyboard to be manipulated
+addKeyboardKey(keyboardKey);
+
+let currentPosition = 0;
+// let upperCase = false;
+const $spanLetters = document.querySelectorAll<HTMLSpanElement>(
+  'span.letter'
+) as NodeListOf<HTMLSpanElement>;
+if ($spanLetters.length === 0) throw new Error('No span.letter elements found');
+keyboardHintOn(currentPosition);
 document.addEventListener('keydown', (event: KeyboardEvent) => {
+  // // Includes the 'Shift' key for capitalized letters
+
+  // // Checks if the letter is capitalized
+  // if (
+  //   $spanLetters[currentPosition].textContent ===
+  //   $spanLetters[currentPosition].textContent?.toLocaleUpperCase()
+  // ) {
+  //   upperCase = true;
+  // }
+
+  /// ///////////
+  // Add Code //
+  /// ///////////
+
+  // Disregards the 'Shift' key
   if (event.key === 'Shift') {
-    console.log('');
-  } else if (event.key === $spanLetters[charNum].textContent) {
-    $spanLetters[charNum].style.textDecoration = 'none';
-    $spanLetters[charNum].style.color = 'rgb(0, 225, 0)';
-    if ($spanLetters[charNum + 1]) {
-      $spanLetters[charNum + 1].style.textDecoration = 'underline';
+    return;
+  }
+
+  // Changes the letter green if the correct key had been pressed
+  if (event.key === $spanLetters[currentPosition].textContent) {
+    $spanLetters[currentPosition].style.textDecoration = '';
+    $spanLetters[currentPosition].style.color = 'rgb(0, 180, 0)';
+    keyboardHintOff(currentPosition);
+
+    if ($spanLetters[currentPosition + 1]) {
+      $spanLetters[currentPosition + 1].style.textDecoration = 'underline';
+      keyboardHintOn(currentPosition + 1);
     }
-    if (accuracyArray.length < charNum + 1) {
+    if (accuracyArray.length < currentPosition + 1) {
       accuracyArray.push(1);
       if (accuracyArray.length === possibleKeys) {
         // Calculates accuracy of typing
@@ -78,7 +272,7 @@ document.addEventListener('keydown', (event: KeyboardEvent) => {
         );
 
         // Displays the final accuracy calculation
-        $accuracy.innerHTML = `You were&nbsp;<div class="accuracy-score">${accuracyScore}%</div>&nbsp;accurate with your typing`;
+        $accuracy.innerHTML = `You were <div class="accuracy-score">${accuracyScore}%</div> accurate with your typing`;
         setTimeout(() => {
           if (confirm('Would you like to try again?')) {
             resetExercise();
@@ -86,83 +280,11 @@ document.addEventListener('keydown', (event: KeyboardEvent) => {
         }, 0);
       }
     }
-    charNum++;
+    currentPosition++;
   } else {
-    $spanLetters[charNum].style.color = 'red';
-    if (accuracyArray.length < charNum + 1) {
+    $spanLetters[currentPosition].style.color = 'red';
+    if (accuracyArray.length < currentPosition + 1) {
       accuracyArray.push(0);
     }
   }
 });
-
-const keyboardKeys: string[] = [
-  '`',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '0',
-  '-',
-  '=',
-  'q',
-  'w',
-  'e',
-  'r',
-  't',
-  'y',
-  'u',
-  'i',
-  'o',
-  'p',
-  '[',
-  ']',
-  '\\',
-  'a',
-  's',
-  'd',
-  'f',
-  'g',
-  'h',
-  'j',
-  'k',
-  'l',
-  ';',
-  `'`,
-  'Shift',
-  'z',
-  'x',
-  'c',
-  'v',
-  'b',
-  'n',
-  'm',
-  ',',
-  '.',
-  '/',
-  'Shift',
-  ' ',
-];
-
-for (let i = 0; i < keyboardKeys.length; i++) {
-  const $divElement = document.createElement('div');
-  $divElement.className = `keyboard-key ${keyboardKeys[i]} flex`;
-  const $h1Element = document.createElement('h1');
-  $h1Element.textContent = keyboardKeys[i].toLocaleUpperCase();
-  $divElement.append($h1Element);
-  if (i < 13 && $numbersRow) {
-    $numbersRow.append($divElement);
-  } else if (i >= 13 && i < 26 && $topRow) {
-    $topRow.append($divElement);
-  } else if (i >= 26 && i < 37 && $middleRow) {
-    $middleRow.append($divElement);
-  } else if (i >= 37 && i < 49 && $bottomRow) {
-    $bottomRow.append($divElement);
-  } else {
-    $spaceRow.append($divElement);
-  }
-}
