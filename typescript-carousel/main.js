db719327b0eb618c138cdee44265aafec6359000
Aaron Kim <ajkim19@gmail.com>
2025-01-31 00:45:41 +0000

    Created working carousel of images with corresponding nav bar

--- typescript-carousel/main.js
+++ typescript-carousel/main.js
@@ -6,40 +6,53 @@ const imageList = [
   'images/025.png',
   'images/039.png',
 ];
+let card = {
+  previous: 4,
+  current: 0,
+};
+// Initializes the count
 let count = 0;
-const cardImage = document.querySelector('.card-image');
-if (!cardImage) throw new Error('cardImage does not exist');
-cardImage.setAttribute('src', imageList[count]);
-const imgNavBar = document.querySelector('.img-nav-bar');
-if (!imgNavBar) throw new Error('imgNavBar does not exist');
-const imgNavBarChildren = imgNavBar.children;
-if (!imgNavBarChildren) throw new Error('imgNavBarChildren does not exist');
-let imgNavBarChildCurrent = imgNavBarChildren[0];
-if (!imgNavBarChildCurrent)
-  throw new Error('imgNavBarChildCurrent does not exist');
-imgNavBarChildCurrent.style.fontWeight = '900';
-let imgNavBarChildPrevious;
-const timerId = setInterval(() => {
-  count++;
+const $cardImage = document.querySelector('.card-image');
+if (!$cardImage) throw new Error('$cardImage does not exist');
+$cardImage.setAttribute('src', imageList[count]);
+const $chevronLeft = document.querySelector('.fa-chevron-left');
+if (!$chevronLeft) throw new Error('$chevronLeft does not exist');
+const $chevronRight = document.querySelector('.fa-chevron-right');
+if (!$chevronRight) throw new Error('$chevronRight does not exist');
+const $imgNavBar = document.querySelector('.img-nav-bar');
+if (!$imgNavBar) throw new Error('$imgNavBar does not exist');
+const $imgNavBarChildren = $imgNavBar.children;
+if (!$imgNavBarChildren) throw new Error('$imgNavBarChildren does not exist');
+let $imgNavBarChildCurrent = $imgNavBarChildren[0];
+if (!$imgNavBarChildCurrent)
+  throw new Error('$imgNavBarChildCurrent does not exist');
+$imgNavBarChildCurrent.style.fontWeight = '900';
+let $imgNavBarChildPrevious;
+function carousel() {
   // Changes the displayed image
-  if (!cardImage) throw new Error('cardImage does not exist');
-  cardImage.setAttribute('src', imageList[count]);
+  if (!$cardImage) throw new Error('$cardImage does not exist');
+  $cardImage.setAttribute('src', imageList[count]);
+  console.log('count:', count);
+  console.log('imageList[count]', imageList[count]);
   // Fills in the circle to the associated image
-  if (!imgNavBarChildren) throw new Error('imgNavBarChildren does not exist');
-  imgNavBarChildCurrent = imgNavBarChildren[count];
-  // Changes class name to make icon a solid circle
-  if (!imgNavBarChildCurrent) throw new Error('imgNavBarChild does not exist');
-  imgNavBarChildCurrent.style.fontWeight = '900';
+  if (!$imgNavBarChildren) throw new Error('$imgNavBarChildren does not exist');
+  $imgNavBarChildCurrent = $imgNavBarChildren[count];
+  // Changes the open circle icon into a solid circle
+  if (!$imgNavBarChildCurrent)
+    throw new Error('$imgNavBarChild does not exist');
+  $imgNavBarChildCurrent.style.fontWeight = '900';
   // Hollows out the previously solid circle
   if (count === 0) {
-    imgNavBarChildPrevious = imgNavBar.children[4];
+    $imgNavBarChildPrevious = $imgNavBarChildren[4];
   } else {
-    imgNavBarChildPrevious = imgNavBar.children[count - 1];
+    $imgNavBarChildPrevious = $imgNavBarChildren[count - 1];
   }
-  if (!imgNavBarChildPrevious)
-    throw new Error('imgNavBarChildPrevious does not exist');
-  imgNavBarChildPrevious.style.fontWeight = '400';
-  if (count === 4) {
-    count = -1;
+  if (!$imgNavBarChildPrevious)
+    throw new Error('$imgNavBarChildPrevious does not exist');
+  $imgNavBarChildPrevious.style.fontWeight = '400';
+  count++;
+  if (count === 5) {
+    count = 0;
   }
-}, 3000);
+}
+let carouselIntervalID = setInterval(carousel, 3000);
--- typescript-carousel/main.ts
+++ typescript-carousel/main.ts
@@ -5,48 +5,61 @@ const imageList = [
   'images/025.png',
   'images/039.png',
 ];
+
+// Initializes the count
 let count = 0;
 
-const cardImage = document.querySelector('.card-image') as HTMLImageElement;
-if (!cardImage) throw new Error('cardImage does not exist');
-cardImage.setAttribute('src', imageList[count]);
+const $cardImage = document.querySelector('.card-image') as HTMLImageElement;
+if (!$cardImage) throw new Error('$cardImage does not exist');
+$cardImage.setAttribute('src', imageList[count]);
+const $chevronLeft = document.querySelector('.fa-chevron-left') as HTMLElement;
+if (!$chevronLeft) throw new Error('$chevronLeft does not exist');
+const $chevronRight = document.querySelector(
+  '.fa-chevron-right'
+) as HTMLElement;
+if (!$chevronRight) throw new Error('$chevronRight does not exist');
+const $imgNavBar = document.querySelector('.img-nav-bar') as HTMLDivElement;
+if (!$imgNavBar) throw new Error('$imgNavBar does not exist');
+const $imgNavBarChildren = $imgNavBar.children;
+if (!$imgNavBarChildren) throw new Error('$imgNavBarChildren does not exist');
+let $imgNavBarChildCurrent = $imgNavBarChildren[0] as HTMLElement;
+if (!$imgNavBarChildCurrent)
+  throw new Error('$imgNavBarChildCurrent does not exist');
+$imgNavBarChildCurrent.style.fontWeight = '900';
+let $imgNavBarChildPrevious: HTMLElement;
 
-const imgNavBar = document.querySelector('.img-nav-bar') as HTMLDivElement;
-if (!imgNavBar) throw new Error('imgNavBar does not exist');
-const imgNavBarChildren = imgNavBar.children;
-if (!imgNavBarChildren) throw new Error('imgNavBarChildren does not exist');
-let imgNavBarChildCurrent = imgNavBarChildren[0] as HTMLElement;
-if (!imgNavBarChildCurrent)
-  throw new Error('imgNavBarChildCurrent does not exist');
-imgNavBarChildCurrent.style.fontWeight = '900';
-
-let imgNavBarChildPrevious: HTMLElement;
-
-setInterval(() => {
-  count++;
+function carousel(): void {
   // Changes the displayed image
-  if (!cardImage) throw new Error('cardImage does not exist');
-  cardImage.setAttribute('src', imageList[count]);
+  if (!$cardImage) throw new Error('$cardImage does not exist');
+  $cardImage.setAttribute('src', imageList[count]);
+  console.log('count:', count);
+  console.log('imageList[count]', imageList[count]);
 
   // Fills in the circle to the associated image
-  if (!imgNavBarChildren) throw new Error('imgNavBarChildren does not exist');
-  imgNavBarChildCurrent = imgNavBarChildren[count] as HTMLElement;
+  if (!$imgNavBarChildren) throw new Error('$imgNavBarChildren does not exist');
+  $imgNavBarChildCurrent = $imgNavBarChildren[count] as HTMLElement;
 
-  // Changes class name to make icon a solid circle
-  if (!imgNavBarChildCurrent) throw new Error('imgNavBarChild does not exist');
-  imgNavBarChildCurrent.style.fontWeight = '900';
+  // Changes the open circle icon into a solid circle
+  if (!$imgNavBarChildCurrent)
+    throw new Error('$imgNavBarChild does not exist');
+  $imgNavBarChildCurrent.style.fontWeight = '900';
 
   // Hollows out the previously solid circle
   if (count === 0) {
-    imgNavBarChildPrevious = imgNavBar.children[4] as HTMLElement;
+    $imgNavBarChildPrevious = $imgNavBarChildren[4] as HTMLElement;
   } else {
-    imgNavBarChildPrevious = imgNavBar.children[count - 1] as HTMLElement;
+    $imgNavBarChildPrevious = $imgNavBarChildren[count - 1] as HTMLElement;
   }
-  if (!imgNavBarChildPrevious)
-    throw new Error('imgNavBarChildPrevious does not exist');
-  imgNavBarChildPrevious.style.fontWeight = '400';
+  if (!$imgNavBarChildPrevious)
+    throw new Error('$imgNavBarChildPrevious does not exist');
+  $imgNavBarChildPrevious.style.fontWeight = '400';
 
-  if (count === 4) {
-    count = -1;
+  count++;
+
+  if (count === 5) {
+    count = 0;
   }
-}, 3000);
+}
+
+// let carouselIntervalID =
+setInterval(carousel, 3000);
